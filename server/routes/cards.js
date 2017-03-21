f5e9e5b7d92accfe0d1d1d53654d934e7e8f4a1c
jonathonlaylo <jonathon.laylo@gmail.com>
2017-03-21 00:44:13 -1000

    get route working but create route showing null values

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,21 +1,34 @@
 /*jshint esversion: 6 */
 const express = require('express');
-const server = require('../server');
 const app =  express();
+const bp = require('body-parser');
 const router = express.Router();
+const db = require('../models');
+const { Card } = db;
 
-router.route('/api/kanban/todo')
+router.use(bp.urlencoded({ extended: true }));
+
+router.route('/')
   .get((req, res) => {
-
+    Card.findAll()
+    .then( cards => {
+      res.json({cardsList:{cards}});
+    });
   })
   .post((req, res) => {
-
+    Card.create({
+      title: req.body.title,
+      priority: req.body.priority,
+      status: req.body.status
+    })
+    .then((task) => {
+      res.send();
+    });
   });
 
 router.route('/new')
   .post((req, res) => {
 
   });
-
 
 module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const app = express();
+const bp = require('body-parser');
+const db = require('./models');
+
+const cardsroute = require('./routes/cards.js');
+
+app.use(bp.urlencoded({ extended: true }));
+
+app.use('/api/kanban/:status', cardsroute);
+
+app.get('/', (req, res) => {
+  res.send('sanity test');
+});
+
+app.listen(8080, (req, res) => {
+  console.log('server started');
+});
+
+module.exports = app;
