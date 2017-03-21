63026956d26c8e207433f262e9490e3933886d79
jonathonlaylo <jonathon.laylo@gmail.com>
2017-03-20 23:34:29 -1000

    working on server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const app = express();
+// const db = require('./models');
 
 app.get('/', (req, res) => {
   res.send('sanity test');
 });
 
-app.listen(3000, (req, res) => {
+app.listen(8080, (req, res) => {
   console.log('server started');
 });
+
+module.exports = app;
